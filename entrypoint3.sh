d827a1daf5e423a55e57825d1eb480fb0728904b
Manoj Kandlikar <manojkandlikar@Manojs-MacBook-Air.local>
2026-03-28 09:35:42 -0700

    rick

--- entrypoint3.sh
+++ entrypoint3.sh
@@ -8,9 +8,9 @@ echo "Starting on PORT=${PORT}"
 
 # 1. Inject env vars into index.html
 sed \
-  -e "s|YOUR_GOOGLE_CLIENT_ID\.apps\.googleusercontent\.com|${LP_GOOGLE_CLIENT_ID}|g" \
-  -e "s|https://buy\.stripe\.com/YOUR_LINK|${LP_STRIPE_LINK}|g" \
-  -e "s|window\.LP_API_BASE || ''|'${LP_API_BASE}'|g" \
+  -e "s|YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com|${LP_GOOGLE_CLIENT_ID}|g" \
+  -e "s|https://buy.stripe.com/YOUR_LINK|${LP_STRIPE_LINK}|g" \
+  -e "s|window.LP_API_BASE || ''|'${LP_API_BASE}'|g" \
   /tmp/index.template.html > /usr/share/nginx/html/index.html
 
 echo "Injected: Google=${LP_GOOGLE_CLIENT_ID:0:20}... Stripe=${LP_STRIPE_LINK:0:35}... API=${LP_API_BASE}"
